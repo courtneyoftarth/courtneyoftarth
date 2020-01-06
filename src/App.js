6cd03a6f8610f5e613a52537f0b6b762c8750130
Courtney <dowelc@cs.washington.edu>
2020-01-05 22:13:12 -0800

    Add table of tweets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Tweets from './Tweets';
 import './App.css';
 
 function App() {
+
+  if (!window.location.href.includes('access')) {
+    window.location.href = 'https://api.stocktwits.com/api/2/oauth/authorize/?' +
+      'client_id=a15329c01d369560' +
+      '&response_type=token' + 
+      '&redirect_uri=https://courtneyoftarth.github.io/stocks' + 
+      '&scope=read,watch_lists,publish_messages,publish_watch_lists,follow_users,follow_stocks';
+  }
+
+  const accessToken = window.location.hash.split('=')[1];
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing the deploy script!
-        </p>
-      </header>
+      <p>
+        Access token is {accessToken}
+      </p>
+      <Tweets />
     </div>
   );
 }
---
+++ src/Tweets.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const TweetsGrid = styled.table`
+    max-width: 700px;
+`;
+
+const TweetMessage = styled.td`
+    padding: 50px;
+`;
+
+const TweetLikes = styled.td`
+    width: 100px;
+`;
+
+const TweetRow = styled.tr`
+
+`;
+
+const Tweets = () => {
+    const [tweets, setTweets] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get('https://api.stocktwits.com/api/2/streams/symbols.json', {
+                params: {
+                    access_token: window.location.hash.split('=')[1],
+                    symbols: 'AAPL,MSFT'
+                }
+            });
+            console.log(response.data.messages);
+            setTweets(response.data.messages || []);
+        })();
+    }, []);
+
+    return (
+        <div>
+            <h1>Tweets about stocks you're tracking</h1>
+            <TweetsGrid>
+                {tweets.map(({ body, likes }) => (
+                    <TweetRow>
+                        <TweetMessage>🗨{body}</TweetMessage>
+                        <TweetLikes>❤ {likes && likes.total || 0}</TweetLikes>
+                    </TweetRow>
+                ))}
+            </TweetsGrid>
+            
+        </div>
+    );
+};
+
+export default Tweets;
