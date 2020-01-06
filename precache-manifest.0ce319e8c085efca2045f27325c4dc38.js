fea5aae02c38ea4fbc6180e42f3b1a9d70d4c9ac
Courtney <dowelc@cs.washington.edu>
2020-01-05 18:57:45 -0800

    Updates

---
+++ precache-manifest.0ce319e8c085efca2045f27325c4dc38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bba21893a0bdf8e9621e8c5f42cee2",
+    "url": "/courtneyoftarth/index.html"
+  },
+  {
+    "revision": "fd2babb1c5e585fbe12a",
+    "url": "/courtneyoftarth/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "97c618312e6feae87609",
+    "url": "/courtneyoftarth/static/js/2.7b29b5ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/courtneyoftarth/static/js/2.7b29b5ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd2babb1c5e585fbe12a",
+    "url": "/courtneyoftarth/static/js/main.bd21d512.chunk.js"
+  },
+  {
+    "revision": "26804beac6142e0a50b9",
+    "url": "/courtneyoftarth/static/js/runtime-main.510cbdfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/courtneyoftarth/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f983458368972dccc4043a8de17dc8f.js"
+  "/courtneyoftarth/precache-manifest.0ce319e8c085efca2045f27325c4dc38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/courtneyoftarth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
